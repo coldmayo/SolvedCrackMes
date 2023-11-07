5e5612e2f3ebb2f7e2da1106b56747a05405b11a
coldmayo <wallachm@msu.edu>
2023-11-07 10:53:26 -0500

    crackme2

---
+++ crackme2/crackme02.c
@@ -0,0 +1,23 @@
+// This is what I assume would have been the source code of crackme02
+
+// the password was: ""
+
+#include <string.h>
+#include <stdio.h>
+
+int main(int argc, char** argv) {
+
+    if (argc != 2) {
+        printf("Need exactly one argument");
+        return 1;
+    }
+
+    if (argv[1][0] == '\0') {
+        printf("Yes %s is correct", argv[1]);
+        return 0;
+    }
+
+    printf("No %s is not correct", argv[1]);
+    return -1;
+
+}
